adba48e2c0d054737ca5c3dd0a9c0434197204c4
rogues <rogues@es6.today>
2017-03-26 00:05:44 -0600

    v0.1.0-beta.1 – Setup (#3)
    0.1.0/SettingUpTheEnvironment
    c3203c3: [setup] Enable Continuous Integration.
     - Ignore unnecessary files and folders.
     - Create package.json for npm.
     - Install node-sass and sass-lint libraries.
     - Add .scss-lint.yml configuration file.
     - Add .travis.yml configuration file.
    116d29b: [build] Automatically run tests before each commit with husky.
    92beda6: [tool] Install EditorConfig on the project.
    7c41233: [tool] Add ES6 support.
    3d6c060: [tool] Add ESLint configuration file and library.
    7e93d7d: [feat] Enable JavaScript Unit Testing.
    8998252: [feat] Enable Sass Unit Testing.
    094e7bc: [docs] Configure SassDoc on project.

---
+++ tests/index.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('Learning ES6 with TDD', () => {
+  it('should be fun', () => {
+    const FUN = true;
+
+    expect(FUN).to.be.true; // eslint-disable-line no-unused-expressions
+  });
+});
